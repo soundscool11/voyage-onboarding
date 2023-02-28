0c09e8e001bbdeb121e4f700c4e2fbb7f6f0e991
Siwon-Kim <siwonkim1108@gmail.com>
2023-02-28 02:53:35 -0500

    python requests practice

--- 3-python,mongoDB/hello.py
+++ 3-python,mongoDB/hello.py
@@ -3,4 +3,9 @@ import requests
 r = requests.get('http://spartacodingclub.shop/sparta_api/seoulair')
 rjson = r.json()
 
-print(rjson)
+rows = rjson['RealtimeCityAir']['row']
+
+for row in rows:
+    province = row['MSRSTE_NM']
+    status = row['IDEX_MVL']
+    print(province, status)
