daaaf3fabb444f5838c364b5ec9ba2b851d2b61e
Siwon-Kim <siwonkim1108@gmail.com>
2023-03-01 18:12:05 -0500

    flask practice

---
+++ 5-aws,deployment/05.fan/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://siwon:rlaznf11@cluster0.icysouv.mongodb.net/?retryWrites=true&w=majority')
+db = client.dbsiwon
+
+@app.route('/')
+def home():
+   return render_template('index.html')
+
+@app.route("/fanbook", methods=["POST"])
+def fanbook_post():
+    nickname_receive = request.form['nickname_give']
+    comment_receive = request.form['comment_give']
+    
+    doc = {
+        'nickname': nickname_receive,
+        'comment': comment_receive
+    }
+    db.fanbooks.insert_one(doc)
+    return jsonify({'msg': 'Your fan message is saved!'})
+
+
+@app.route("/fanbook", methods=["GET"])
+def fanbook_get():
+    all_fanbooks = list(db.fanbooks.find({}, {'_id': False}))
+    return jsonify({'msg': all_fanbooks})
+
+if __name__ == '__main__':
+   app.run('0.0.0.0', port=5000, debug=True)
