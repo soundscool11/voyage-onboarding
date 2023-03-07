923c3dee8f0eb4e4855be7b804bdf5a8543cf973
Siwon-Kim <siwonkim1108@gmail.com>
2023-03-06 19:25:49 -0500

    day 1 Q4

--- algorithms/day-1/10250-ACM-hotel.py
+++ algorithms/day-1/10250-ACM-hotel.py
@@ -32,14 +32,15 @@ for _ in range(T):
     H, W, N = map(int, input().split())
 
     count = 0
-    for i in range(W):
-        for j in range(H):
+    for i in range(1, W+1):
+        for j in range(1, H+1):
             count += 1
             if count == N:
-                if j < 10:
-                    print(f"{i}0{j}")
+                if i < 10:
+                    print(f"{j}0{i}")
                 else:
-                    print(f"{i}{j}")
+                    print(f"{j}{i}")
+                break
 
 
 end_time = time.time()
