2c2f83cd77759e6063d39a119874bcbd12661eb4
Siwon-Kim <siwonkim1108@gmail.com>
2023-03-08 06:36:38 -0500

    algorithms day 3

--- algorithms/day-3/11279-max-heap.py
+++ algorithms/day-3/11279-max-heap.py
@@ -2,6 +2,20 @@ import sys, time
 start_time = time.time()
 sys.stdin = open("input.txt", "r")
 
+import heapq, sys
+heap = []
+heapq.heapify(heap)
+input = sys.stdin.readline
+N = int(input())
+for _ in range(N):
+    i = int(input())
+    if i == 0:
+        if len(heap) < 1:
+            print(0)
+        else:
+            print(-heapq.heappop(heap))
+    else:
+        heapq.heappush(heap, -i)
 
 
 end_time = time.time()
