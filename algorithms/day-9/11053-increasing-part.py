5d345d74313f51f88c38996bc9102c8f0aec1a91
Siwon-Kim <siwonkim1108@gmail.com>
2023-03-20 07:58:21 -0400

    increasing part

--- algorithms/day-9/11053-increasing-part.py
+++ algorithms/day-9/11053-increasing-part.py
@@ -1,3 +1,15 @@
 import sys
 sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
+
+n = int(input())
+perm = list(map(int, input().split()))
+mem = [1] * n
+
+for i in range(1, n):
+    for j in range(i):
+        if perm[i] > perm[j]:
+            mem[i] = max(mem[i], mem[j]+1)
+
+print(max(mem))
+    
