d355605e28f349dce5a8a705ae18d629163ead26
Siwon-Kim <siwonkim1108@gmail.com>
2023-03-21 06:39:16 -0400

    algorithms day 9 dp

--- algorithms/day-9/2579-stair.py
+++ algorithms/day-9/2579-stair.py
@@ -1,3 +1,26 @@
 import sys
 sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
+
+n = int(input())
+steps = []
+mem = [0] * n
+
+for _ in range(n):
+    steps.append(int(input()))
+
+if n == 1:
+    print(steps[0])
+
+elif n == 2:
+    print(steps[0]+steps[1])
+
+else:
+    mem[0] = steps[0]
+    mem[1] = steps[0] + steps[1]
+    mem[2] = max(steps[0] + steps[2], steps[1] + steps[2])
+
+    for i in range(3, n):
+        mem[i] = max(mem[i-3] + steps[i-1] + steps[i], mem[i-2] + steps[i])
+
+    print(mem[n-1])
