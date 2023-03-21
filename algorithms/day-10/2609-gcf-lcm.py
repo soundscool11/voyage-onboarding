726141de4325de847e675b625b9267a19be6d3db
Siwon-Kim <siwonkim1108@gmail.com>
2023-03-21 08:00:58 -0400

    gcd lcm

--- algorithms/day-10/2609-gcf-lcm.py
+++ algorithms/day-10/2609-gcf-lcm.py
@@ -1,3 +1,9 @@
-import sys
+import sys, math
 sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
+
+a, b = map(int, input().split())
+
+gcd = math.gcd(a, b)
+print(gcd)
+print(a * b // gcd)
