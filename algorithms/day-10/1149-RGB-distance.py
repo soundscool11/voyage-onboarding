aa5c8a1eaef829777395bae1881fd08815651ea6
Siwon-Kim <siwonkim1108@gmail.com>
2023-03-22 14:14:43 -0400

    RGB distance problems

--- algorithms/day-10/1149-RGB-distance.py
+++ algorithms/day-10/1149-RGB-distance.py
@@ -1,3 +1,16 @@
 import sys
 sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
+
+N = int(input())
+costs = []
+
+for _ in range(N):
+    costs.append(list(map(int, input().split())))
+
+for i in range(1, N):
+    costs[i][0] = min(costs[i-1][1], costs[i-1][2]) + costs[i][0]
+    costs[i][1] = min(costs[i-1][0], costs[i-1][2]) + costs[i][1]
+    costs[i][2] = min(costs[i-1][0], costs[i-1][1]) + costs[i][2]
+
+print(min(costs[N-1]))
