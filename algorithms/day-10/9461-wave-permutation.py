440efaf5b4f11abb99e8438df121517ca094c7bb
Siwon-Kim <siwonkim1108@gmail.com>
2023-03-22 09:16:20 -0400

    permutation pattern dp

--- algorithms/day-10/9461-wave-permutation.py
+++ algorithms/day-10/9461-wave-permutation.py
@@ -1,3 +1,17 @@
 import sys
 sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
+
+tri = [0 for i in range(101)]
+tri[1] = 1
+tri[2] = 1
+tri[3] = 1
+
+for i in range(4, 101):
+    tri[i] = tri[i-3] + tri[i-2]
+
+T = int(input())
+
+for _ in range(T):
+    N = int(input())
+    print(tri[N])
