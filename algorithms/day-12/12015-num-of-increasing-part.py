f49579c1e32139cc09b0d74fb5d97cb2a72b18ad
Siwon-Kim <siwonkim1108@gmail.com>
2023-03-23 07:05:41 -0400

    LCS binary search

--- algorithms/day-12/12015-num-of-increasing-part.py
+++ algorithms/day-12/12015-num-of-increasing-part.py
@@ -3,6 +3,23 @@ sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
 
 n = int(input())
-arr = list(map(int, input().split()))
+cases = list(map(int, input().split()))
+lst = [0]
 
+for case in cases:
+    if lst[-1] < case:
+        lst.append(case)
 
+    else:
+        start = 0
+        end = len(lst)
+
+        while start < end:
+            mid = (start + end) // 2
+            if lst[mid] < case:
+                start = mid + 1
+            else:
+                end = mid
+        lst[end] = case
+
+print(len(lst) - 1)
--- algorithms/day-12/1300-Kth-value.py
+++ algorithms/day-12/1300-Kth-value.py
@@ -2,4 +2,21 @@ import sys
 sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
 
-n = int(input())
+n, k = int(input()), int(input())
+start, end = 1, k
+
+while start <= end:
+    mid = (start + end) // 2
+    temp = 0
+
+    for i in range(1, n+1):
+        temp += min(mid // i, n)
+
+    if temp >= k:
+        answer = mid
+        end = mid - 1
+    else:
+        start = mid + 1
+
+print(answer)
+    
